5abb604b1be1a0ec3de13f4dda7c68891c4633c2
Yusuf <yakande96@gmail.com>
2023-07-18 15:21:50 +0100

    fixed creator status authorization bug

--- controllers/creatorController.js
+++ controllers/creatorController.js
@@ -61,14 +61,9 @@ const updateCreatorStatus =  async (req, res, next) => {
       let  status  = req.body.status;
       const id = req.params.id;
   
-      // const oldCreator = await Creator.findById(id);
-  
-      // Checking if the user attempting to update is the author 
-      // if (req.user._id.toString() !== oldQuestion.creatorId._id.toString()) {
-      //   return next(
-      //     new AppError("You cannot edit as you're not the author", 403)
-      //   );
-      // } 
+      if (req.user.role !== "admin") {
+        return new AppError("You are not authorized", 403)
+      }
   
       if (
         !(
